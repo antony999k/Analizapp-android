3294a12593d7845343a5500bba9c33d9535a74f9
Antony999k <lv999k@hotmail.com>
2018-11-24 01:57:49 -0600

    :art: mejora xml del menu

--- app/src/main/java/com/example/lv999k/analizapp/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/lv999k/analizapp/fragments/SettingsFragment.java
@@ -3,6 +3,7 @@ package com.example.lv999k.analizapp.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,19 +14,11 @@ import com.example.lv999k.analizapp.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
-
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.pref_general, rootKey);
     }
 
 }
