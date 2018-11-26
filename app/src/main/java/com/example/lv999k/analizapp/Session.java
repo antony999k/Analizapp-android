a83d82f27dc69aa870dc1a1d87aaccf8408674be
javicuriel <javiercuriel94@gmail.com>
2018-11-26 02:53:54 -0600

    Added session

--- app/src/main/java/com/example/lv999k/analizapp/Session.java
+++ app/src/main/java/com/example/lv999k/analizapp/Session.java
@@ -1,8 +1,23 @@
 package com.example.lv999k.analizapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by javiercuriel on 11/26/18.
  */
 
 public class Session {
+    private static String PREF_NAME = "auth";
+    private static String ID = "token";
+
+    public static boolean saveSessionID(String token, Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE).edit();
+        editor.putString(ID , token);
+        return editor.commit();
+    }
+    public static String getSessionID(Context context) {
+        SharedPreferences savedSession = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        return savedSession.getString(ID , null);
+    }
 }
