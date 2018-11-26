2ef78862d32907e594b2a19681b52b40748ffee5
javicuriel <javiercuriel94@gmail.com>
2018-11-26 02:53:33 -0600

    Added DB classes

--- app/src/main/java/com/example/lv999k/analizapp/Experiment.java
+++ app/src/main/java/com/example/lv999k/analizapp/Experiment.java
@@ -1,11 +1,25 @@
-package com.example.lv999k.analizapp.fragments;
+package com.example.lv999k.analizapp;
+
+import java.util.Date;
 
 /**
  * Created by javiercuriel on 11/26/18.
  */
 
 public class Experiment {
+    Integer id;
+    String nombre;
+    String descripcion;
+    Integer usuario_id;
+    Date fecha;
 
+    public Experiment(Integer id, String nombre, String descripcion, Integer usuario_id, Date fecha) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.usuario_id = usuario_id;
+        this.fecha = fecha;
+    }
 }
 
 
--- app/src/main/java/com/example/lv999k/analizapp/Metal.java
+++ app/src/main/java/com/example/lv999k/analizapp/Metal.java
@@ -5,4 +5,23 @@ package com.example.lv999k.analizapp;
  */
 
 public class Metal {
+    Integer id;
+    String nombre;
+    String descripcion;
+
+    public Metal(Integer id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
 }
+
+
+
+//    create table Metal(
+//        id int not null AUTO_INCREMENT,
+//        nombre varchar(100) not null,
+//        descripcion varchar(200) default '',
+//        primary key (id)
+//        );
