1367a1f3cf7114fa9b1da02b6b157148c7eb8e0f
Antony999k <lv999k@hotmail.com>
2018-11-28 16:01:35 -0600

    :sparkles: Chart ejemplo añadido

--- app/src/main/java/com/example/lv999k/analizapp/fragments/ImageInfoFragment.java
+++ app/src/main/java/com/example/lv999k/analizapp/fragments/ImageInfoFragment.java
@@ -3,6 +3,8 @@ package com.example.lv999k.analizapp.fragments;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,14 +13,27 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lv999k.analizapp.Principal;
+import com.github.mikephil.charting.data.Entry;
 import com.example.lv999k.analizapp.R;
+import com.example.lv999k.analizapp.adapters.ImagesAdapter;
 import com.example.lv999k.analizapp.bo.Image;
 import com.example.lv999k.analizapp.services.ApiService;
 import com.example.lv999k.analizapp.utils.CustomResponse;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.Highlight;
+import com.github.mikephil.charting.utils.PercentFormatter;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -36,10 +51,10 @@ public class ImageInfoFragment extends Fragment {
     ImageView imageView;
     TextView titleView;
     TextView descriptionView;
-
     ProgressBar progressBar;
+    PieChart area_chart;
 
-
+    @SuppressWarnings("FieldCanBeLocal")
     public ImageInfoFragment() {
         // Required empty public constructor
     }
@@ -73,9 +88,12 @@ public class ImageInfoFragment extends Fragment {
         imageView = view.findViewById(R.id.imageView);
         titleView = view.findViewById(R.id.title);
         descriptionView = view.findViewById(R.id.description);
+        area_chart = (PieChart) view.findViewById(R.id.area_chart);
 
         setImageInfo();
         loadImage();
+
+        addData();
 
         return view;
     }
@@ -105,7 +123,36 @@ public class ImageInfoFragment extends Fragment {
         });
     }
 
+    private void addData() {
+        //Set chart
+        ArrayList NoOfEmp = new ArrayList();
+        NoOfEmp.add(new Entry(945f, 0));
+        NoOfEmp.add(new Entry(1040f, 1));
+        NoOfEmp.add(new Entry(1133f, 2));
+        NoOfEmp.add(new Entry(1240f, 3));
+        NoOfEmp.add(new Entry(1369f, 4));
+        NoOfEmp.add(new Entry(1487f, 5));
+        NoOfEmp.add(new Entry(1501f, 6));
+        NoOfEmp.add(new Entry(1645f, 7));
+        NoOfEmp.add(new Entry(1578f, 8));
+        NoOfEmp.add(new Entry(1695f, 9));
+        PieDataSet dataSet = new PieDataSet(NoOfEmp, "Number Of Employees");
 
-
+        ArrayList year = new ArrayList();
+        year.add("2008");
+        year.add("2009");
+        year.add("2010");
+        year.add("2011");
+        year.add("2012");
+        year.add("2013");
+        year.add("2014");
+        year.add("2015");
+        year.add("2016");
+        year.add("2017");
+        PieData data = new PieData(year, dataSet);
+        area_chart.setData(data);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        area_chart.animateXY(5000, 5000);
+    }
 
 }
