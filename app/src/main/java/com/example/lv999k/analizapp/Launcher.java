7fe27ab7c856cc6851ab439d424ecc536635b828
Antony999k <lv999k@hotmail.com>
2018-11-28 13:23:35 -0600

    :spakles: Splash screen

--- app/src/main/java/com/example/lv999k/analizapp/Launcher.java
+++ app/src/main/java/com/example/lv999k/analizapp/Launcher.java
@@ -6,31 +6,44 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Launcher extends AppCompatActivity {
+
+    private long tiempoDeEspera = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        SharedPreferences pref = getSharedPreferences("auth",Context.MODE_PRIVATE);
-        if(pref.contains("token")){
-            final String token = pref.getString("token", "");
-            //Revisa si el token no esta vacio o es diferente de nulo
-            if(!token.isEmpty() || token != null){
-                startActivity(new Intent(Launcher.this, Principal.class));
-                finish();
-            }else{
-                startActivity(new Intent(Launcher.this, Login.class));
-                finish();
+        TimerTask tarea = new TimerTask(){
+            public void run(){
+                SharedPreferences pref = getSharedPreferences("auth",Context.MODE_PRIVATE);
+                if(pref.contains("token")){
+                    final String token = pref.getString("token", "");
+                    //Revisa si el token no esta vacio o es diferente de nulo
+                    if(!token.isEmpty() || token != null){
+                        startActivity(new Intent(Launcher.this, Principal.class));
+                        finish();
+                    }else{
+                        startActivity(new Intent(Launcher.this, Login.class));
+                        finish();
+                    }
+                }else{
+                    startActivity(new Intent(Launcher.this, Login.class));
+                    finish();
+                }
             }
-        }else{
-            startActivity(new Intent(Launcher.this, Login.class));
-            finish();
-        }
+        };
 
-
+        Timer timer = new Timer();
+        timer.schedule(tarea, tiempoDeEspera);
 
     }
 }
